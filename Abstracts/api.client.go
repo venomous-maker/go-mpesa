ade48f3886e568946f2c804bf22e207db79a3637
venomous-maker <morganokumu8+1@gmail.com>
2026-03-04 11:46:53 +0300

    feat: improve HTTP client configuration with enhanced timeout settings and proxy support

--- Abstracts/api.client.go
+++ Abstracts/api.client.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
+	"net"
 	"net/http"
 	"time"
 )
@@ -99,14 +100,19 @@ func (client *ApiClient) sendRequest(payload any, endpoint, token string, isRetr
 	req.Header.Set("Authorization", "Bearer "+token)
 
 	transport := &http.Transport{
-		TLSHandshakeTimeout:   10 * time.Second,
-		ResponseHeaderTimeout: 15 * time.Second,
+		Proxy: http.ProxyFromEnvironment,
+		DialContext: (&net.Dialer{
+			Timeout:   15 * time.Second,
+			KeepAlive: 30 * time.Second,
+		}).DialContext,
+		TLSHandshakeTimeout:   15 * time.Second,
+		ResponseHeaderTimeout: 40 * time.Second,
 		ExpectContinueTimeout: 1 * time.Second,
-		ForceAttemptHTTP2:     false, // 🔥 Important
+		ForceAttemptHTTP2:     false,
 	}
 
 	clientHTTP := &http.Client{
-		Timeout:   30 * time.Second,
+		Timeout:   60 * time.Second,
 		Transport: transport,
 	}
 
